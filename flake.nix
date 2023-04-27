8c60eceb9d791f8719239a9575437b13f1daaca5
Shikiyo <shikiyo@archjk>
2023-04-27 17:40:53 -0300

    flake: Use flake-utils.lib.eachDefaultSystem

--- flake.nix
+++ flake.nix
@@ -6,42 +6,41 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: {
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        myShell = input@{
+            name,
+            buildInputs ? [],
+            toShellName ? n: "${n}-shell",
+            shellHook ? { shellName, ... }: ''
+              export PS1="\n\[\033[1;32m\][${shellName}: \w]\n\$\[\033[0m\] "
+              export PURE="$([[ $IN_NIX_SHELL = 'pure' ]] && echo 1 || echo 0)"
+              echo "PURE=$PURE"
+              echo -n '>> Welcome to ${shellName}! <<'
+            '',
+          }: pkgs.mkShell {
+            name = toShellName name;
 
-    devShells.x86_64-linux = let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      inself = self.devShells.x86_64-linux;
-      myShell = input@{
-          name,
-          buildInputs ? [],
-          toShellName ? n: "${n}-shell",
-          shellHook ? { shellName, ... }: ''
-            export PS1="\n\[\033[1;32m\][${shellName}: \w]\n\$\[\033[0m\] "
-            export PURE="$([[ $IN_NIX_SHELL = 'pure' ]] && echo 1 || echo 0)"
-            echo "PURE=$PURE"
-            echo -n '>> Welcome to ${shellName}! <<'
-          '',
-        }: pkgs.mkShell {
-          name = toShellName name;
+            buildInputs = buildInputs;
 
-          buildInputs = buildInputs;
+            shellHook = shellHook
+              (input // { shellName = toShellName name; });
+          };
+      in {
+        devShells = {
+            default = self.devShells.${system}.julia;
 
-          shellHook = shellHook
-            (input // { shellName = toShellName name; });
-        };
-    in {
-      default = inself.julia;
+            julia = myShell {
+              name = "Julia";
+              buildInputs = with pkgs; [ julia ];
+            };
 
-      julia = myShell {
-        name = "Julia";
-        buildInputs = with pkgs; [ julia ];
-      };
-
-      j = myShell {
-        name = "j";
-        buildInputs = with pkgs; [ j ];
-      };
-    };
-
-  };
+            j = myShell {
+              name = "j";
+              buildInputs = with pkgs; [ j ];
+            };
+          };
+      });
 }
