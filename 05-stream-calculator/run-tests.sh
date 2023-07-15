f777cec90e0675f3f260561ed4fe30d10539a122
Shikiyo <shikiyo@archjk>
2023-07-15 19:12:32 -0300

    05: run-tests count errors

--- 05-stream-calculator/run-tests.sh
+++ 05-stream-calculator/run-tests.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-set -xe
+set -x
 
 . ./flags.env
 
+total_errs=0
 compile_run () {
   # -Wno-unused-local-typedefs:
   # to use compile-time assert inside funtions
@@ -11,6 +12,15 @@ compile_run () {
   prog="tests/${1}.c"
   out="tests/${1}.out"
   ${compile} "${prog}" -o "${out}" && ./"${out}"
+  total_errs=$((${total_errs} + $?))
 }
 compile_run test_streams
 compile_run test_parser
+
+set +x
+
+if [ ${total_errs} -eq 0 ]; then
+  echo "Summary: All ok!"
+else
+  echo "Summary: Found ${total_errs} in all tests!"
+fi
